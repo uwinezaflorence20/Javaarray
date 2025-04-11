80582ae924509a80af5e01dc95f9e17d1d98c5e8
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-04-11 12:01:59 +0200

    introduction

---
+++ src/Exampleofmap.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap
+public class Exampleofmap {
+
+    public static void main(String[] args) {
+        // declaration
+        Map<String,Integer> studentGrades;
+        Map<Integer,String> EmployeesName;
+
+        //initialization
+        studentGrades = new HashMap<>();
+        EmployeesName = new TreeMap<>();
+
+
+
+    }
+}
