5ff1abb1431e659198a9e6668c86ee8f1720d0c6
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-04-11 12:27:47 +0200

    revision done next example

--- src/Exampleofmap.java
+++ src/Exampleofmap.java
@@ -1,6 +1,6 @@
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.HashMap
+import java.util.HashMap;
 public class Exampleofmap {
 
     public static void main(String[] args) {
@@ -12,6 +12,48 @@ public class Exampleofmap {
         studentGrades = new HashMap<>();
         EmployeesName = new TreeMap<>();
 
+studentGrades.put("Milla",100);
+studentGrades.put("Florence",80);
+studentGrades.put("James",90);
+studentGrades.put("John",80);
+
+EmployeesName.put(1,"Eric");
+EmployeesName.put(2,"Stella");
+
+//accessing the element we use get method
+System.out.println(studentGrades.get("Milla"));
+System.out.println(studentGrades.get("Florence"));
+
+System.out.println(EmployeesName.get(1));
+System.out.println(EmployeesName.get(2));
+
+//updating or modifying  the element we use put
+studentGrades.put("florence",120);
+System.out.println(studentGrades.get( "florence"));
+
+// getting the size of the mapp
+
+        System.out.println( "the size of the Student Hashmap:"+studentGrades.size());
+        System.out.println( "the size of the Employee Treemap:"+EmployeesName.size());
+
+// iteration in map
+
+        for( String name :studentGrades.keySet()){
+            System.out.println( name + " : " + studentGrades.get(name));
+        }
+        for(int employees :EmployeesName.keySet()){
+            System.out.println( employees + " : " + EmployeesName.get(employees));
+        }
+
+//removing the student from the map we use remove method and contain,isEmpty
+studentGrades.remove("james");
+       System.out.println(studentGrades.containsKey("Florence"));
+       System.out.println(EmployeesName.isEmpty());
+       System.out.println(studentGrades.isEmpty());
+       //.clear method is used to remove all the key value pairs form the map
+
+       System.out.println(studentGrades);
+       System.out.println(EmployeesName);
 
 
     }
