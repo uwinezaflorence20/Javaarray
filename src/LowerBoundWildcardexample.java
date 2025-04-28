3c0ef9aaa346454bffa9739f33cdc0b8acd6dc05
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-04-28 15:40:01 +0200

    done again

--- src/LowerBoundWildcardexample.java
+++ src/LowerBoundWildcardexample.java
@@ -1,2 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class LowerBoundWildcardexample {
+   public static void addIntegerToList(List<? super Integer> list) {
+       list.add(100);
+       list.add(200);
+   }
+    public static void main(String[] args){
+       List<Number> numberList = new ArrayList<>();
+       addIntegerToList(numberList);
+       System.out.println("Integer List: "+numberList);
+
+       List<Number> numberList2 = new ArrayList<>();
+       addIntegerToList(numberList2);
+       System.out.println("Number List: "+numberList2);
+
+
+        List<Object> objectList = new ArrayList<>();
+        addIntegerToList(objectList);
+        System.out.println("ObjectList: " +objectList);
+    }
 }
+
