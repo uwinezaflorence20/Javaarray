df2dee97ec090cd30082861fe125ba2003477be1
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-04-28 14:56:29 +0200

    done again

--- src/UpperBoundedWildcarExample.java
+++ src/UpperBoundedWildcarExample.java
@@ -1,2 +1,21 @@
+import java.util.List;
+
 public class UpperBoundedWildcarExample {
+    public static double printList(List<? extends Number> list) {
+        double sum = 0.0;
+        for (Number n : list) {
+            sum += n.doubleValue();
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integerList = List.of(10, 20, 30);
+        double sum1 = printList(integerList); // fixed here
+        System.out.println("Sum: " + sum1);
+
+        List<Double> doubleList = List.of(1.5,2.5);
+        double sum2 = printList(doubleList);
+        System.out.println("Sum: " + sum2);
+    }
 }
