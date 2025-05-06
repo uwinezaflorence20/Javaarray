95ec29446c0184fb5aae6b84891c08fcd4a8983e
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-05-06 14:34:56 +0200

    done

---
+++ src/StreamCalculationExample.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+import java.util.List;
+import java.util.stream.*;
+public class StreamCalculationExample {
+    public static void main(String[] args) {
+        //creating the stream
+        List<String> names = Arrays.asList("paccy","paccy","fillette","zero","phina");
+        Stream<String> stream = names.stream();
+        System.out.println("the stream of the names: "+stream);
+
+        // creating the stream and using the fileter
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        System.out.println("the origin  numbers: "+numbers);
+
+        List<Integer> filteredNumber = numbers.stream()
+                .filter(m->m%2 == 0)
+                .toList();
+        System.out.println("the filtered numbers: "+filteredNumber);
+
+        List<Integer> anotherFiltered = numbers.stream()
+                .filter(n->n%2!=0)
+                .collect(Collectors.toList());
+        System.out.println("the another filtered numbers: "+anotherFiltered);
+
+        //map examples it change somethingon the elements
+        List<String> words = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println("the words in upper case: "+words);
+
+    }
+}
